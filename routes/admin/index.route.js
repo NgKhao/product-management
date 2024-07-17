4c237c95c1e9aa40d6d8bba88416018e7a403aee
NgKhao <bsbnk141@gmail.com>
2024-07-17 13:29:25 +0700

    add GUI products list

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,11 +1,16 @@
 const systemConfig = require("../../config/system");
 
-const dashboardRoutes = require("./dashboard.route")
+const dashboardRoutes = require("./dashboard.route");
+
+const productRoutes = require("./product.route");
+
 
 
 module.exports = (app) => {
     const PATH_ADMIN = systemConfig.prefixAmin;
 
     app.use(PATH_ADMIN + "/dashboard", dashboardRoutes);
+
+    app.use(PATH_ADMIN + "/products", productRoutes);
 
 }
