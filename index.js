11dde376d7f832a5a5b5ccdaaf1cb8e908b98b15
NgKhao <bsbnk141@gmail.com>
2024-07-17 10:27:11 +0700

    buit common layouts

--- index.js
+++ index.js
@@ -9,11 +9,16 @@ const route = require("./routes/client/index.route");
 
 database.connect();
 
+const systemConfig = require("./config/system");
+
 const app = express();
 const port = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+// app Locals Variables
+app.locals.prefixAmin = systemConfig.prefixAmin;
 
 app.use(express.static("public"));
 
