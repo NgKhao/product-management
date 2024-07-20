45ca336a13e457adcbebead5e290e03b46226ee6
NgKhao <bsbnk141@gmail.com>
2024-07-21 00:34:37 +0700

    implement pagination

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -22,9 +22,30 @@ module.exports.index = async (req, res) => {
 
     if(objectSearch.regex){
         find.title = objectSearch.regex;
+
     }
 
-    const products = await Product.find(find);
+    // Pagination
+    let objectPagination = {
+        currentPage: 1,
+        limitItem: 4
+    }
+
+    if(req.query.page) {
+        objectPagination.currentPage = parseInt(req.query.page);
+    }
+
+    // công thức tính index sp  bắt đầu của trang được chọn
+    objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItem;
+
+    const countProducts = await Product.countDocuments(find);
+    const totalPage = Math.ceil(countProducts/objectPagination.limitItem); //làm tròn lên
+    objectPagination.totalPage = totalPage;
+
+    // End Pagination
+
+
+    const products = await Product.find(find).limit(objectPagination.limitItem).skip(objectPagination.skip);
 
     // console.log(products)
 
@@ -32,6 +53,7 @@ module.exports.index = async (req, res) => {
         pageTitle: "Danh sách sản phẩm",
         products: products,
         filterStatus: filterStatus,
-        keyword: objectSearch.keyword
+        keyword: objectSearch.keyword,
+        pagination: objectPagination
     });
 };
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -44,3 +44,22 @@ if(formSearch) {
     })
 }
 // End Form Search
+
+// Pagination
+const buttonPagination = document.querySelectorAll("[button-pagination]"); //thuộc tính tự định nghĩa thì thêm []
+if(buttonPagination) {
+    let url = new URL(window.location.href);
+
+    buttonPagination.forEach(button => {
+        button.addEventListener("click", () => {
+            const page = button.getAttribute("button-pagination");
+            console.log(page);
+
+            url.searchParams.set("page", page);
+
+            window.location.href = url.href;
+
+        });
+    });
+}
+// End Pagination
