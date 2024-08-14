24bcdbb59392399f8e2a48ce3f6c8b1df63dc24f
NgKhao <bsbnk141@gmail.com>
2024-08-14 20:08:50 +0700

    add edit product category

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,4 +1,4 @@
-const ProductCategory = require("../../models/products-category.model")
+const ProductCategory = require("../../models/products-category.model");
 
 const systemConfig = require("../../config/system");
 
@@ -7,34 +7,33 @@ const createTreeHelper = require("../../helpers/createTree");
 // [GET] /admin/products-category
 module.exports.index = async (req, res) => {
   let find = {
-    deleted: false
+    deleted: false,
   };
 
   const records = await ProductCategory.find(find);
 
   const newRecords = createTreeHelper.tree(records);
 
-
   res.render("admin/pages/products-category/index", {
-      pageTitle: "Danh mục sản phẩm",
-      records: newRecords
+    pageTitle: "Danh mục sản phẩm",
+    records: newRecords,
   });
 };
 
 // [GET] /admin/products-category/create
 module.exports.create = async (req, res) => {
   let find = {
-    deleted: false
+    deleted: false,
   };
 
   // tạo cây để phân nhánh danh mục
   function createTree(arr, parentId = "") {
     const tree = [];
     arr.forEach((item) => {
-      if(item.parent_id === parentId){
+      if (item.parent_id === parentId) {
         const newItem = item;
         const children = createTree(arr, item.id);
-        if (children.length > 0){
+        if (children.length > 0) {
           newItem.children = children;
         }
         tree.push(newItem);
@@ -49,16 +48,15 @@ module.exports.create = async (req, res) => {
 
   // console.log(newRecords);
 
-
   res.render("admin/pages/products-category/create", {
-      pageTitle: "Tạo danh mục sản phẩm",
-      records: newRecords
+    pageTitle: "Tạo danh mục sản phẩm",
+    records: newRecords,
   });
 };
 
 // [POST] /admin/products-category/create
 module.exports.createPost = async (req, res) => {
-  if(req.body.position == "") {
+  if (req.body.position == "") {
     const count = await ProductCategory.countDocuments();
     req.body.position = count + 1;
   } else {
@@ -71,4 +69,45 @@ module.exports.createPost = async (req, res) => {
   // prefixAdmin khai báo local trong index chỉ dùng dc trong toàn bộ file pug
   // còn file js phải khai bao request mới dùng đc
   res.redirect(`${systemConfig.prefixAdmin}/products-category`);
+};
+
+// [GET] /admin/products-category/edit/:id
+module.exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const data = await ProductCategory.findOne({
+      _id: id,
+      deleted: false,
+    });
+
+    // console.log(data);
+
+    const records = await ProductCategory.find({
+      deleted: false,
+    });
+
+    // console.log(records);
+
+    const newRecords = createTreeHelper.tree(records);
+
+    res.render("admin/pages/products-category/edit", {
+      pageTitle: "Chỉnh sửa danh mục sản phẩm",
+      data: data,
+      records: newRecords,
+    });
+  } catch (error) {
+    res.redirect(`${systemConfig.prefixAdmin}/products-category`);
+  }
+};
+
+// [PATCH] /admin/products-category/edit/:id
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id;
+
+  req.body.position = parseInt(req.body.position);
+
+  await ProductCategory.updateOne({ _id: id }, req.body);
+
+  res.redirect("back");
 };
