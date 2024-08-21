14d96b76739ed716d94ac288fe4752dc585b3c74
NgKhao <bsbnk141@gmail.com>
2024-08-21 23:36:44 +0700

    add login

---
+++ controllers/admin/auth.controller.js
@@ -0,0 +1,48 @@
+const md5 = require("md5"); // thư viện mã hóa password
+
+const Account = require("../../models/account.model");
+
+const systemConfig = require("../../config/system");
+
+// [GET] /admin/auth/login
+module.exports.login = (req, res) => {
+  res.render("admin/pages/auth/login", {
+    pageTitle: "Trang đăng nhập",
+  });
+};
+
+// [POST] /admin/auth/login
+module.exports.loginPost = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const user = await Account.findOne({
+    email: email,
+    deleted: false,
+  });
+
+  // check email not exist in DB
+  if(!user) {
+    req.flash("error", "Email không tồn tại");
+    res.redirect("back");
+    return;
+  }
+
+  // check password incorrect
+  if(md5(password) != user.password) {
+    req.flash("error", "Sai mật khẩu!");
+    res.redirect("back");
+    return;
+  }
+
+  // check lock account
+  if(user.status == "inactive") {
+    req.flash("error", "Tài khoản đã bị khóa!");
+    res.redirect("back");
+    return;
+  }
+  // console.log(password);
+
+  res.cookie("token", user.token);
+  res.redirect(`${systemConfig.prefixAdmin}/dashboard`);
+};
---
+++ validates/admin/auth.validate.js
@@ -0,0 +1,16 @@
+// hàm này khi bắt lỗi khi user dùng f12 xóa đi require trong tiêu đề
+module.exports.loginPost = (req, res, next) => {
+  if(!req.body.email){
+    req.flash("error", "Vui lòng nhập email!");
+    res.redirect("back");
+    return;
+  }
+
+  if(!req.body.password){
+    req.flash("error", "Vui lòng nhập mật khẩu!");
+    res.redirect("back");
+    return;
+  }
+  
+  next(); // next sang bước kế tiếp
+}
