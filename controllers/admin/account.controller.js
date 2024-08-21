83605d9a0f456abc237c209ee07d5fd6539dccaf
NgKhao <bsbnk141@gmail.com>
2024-08-21 21:14:35 +0700

    add edit an account

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -63,3 +63,59 @@ module.exports.createPost = async (req, res) => {
     res.redirect(`${systemConfig.prefixAdmin}/accounts`);
   }
 };
+
+// [GET] /admin/accounts/edit/:id
+module.exports.edit = async (req, res) => {
+  const find = {
+    _id: req.params.id,
+    deleted: false,
+  };
+
+  try {
+    const data = await Account.findOne(find);
+
+    const roles = await Role.find({
+      deleted: false,
+    });
+
+    // console.log(data);
+
+    res.render("admin/pages/accounts/edit", {
+      pageTitle: "Chỉnh sửa tài khoản",
+      data: data,
+      roles: roles,
+    });
+  } catch (error) {
+    res.redirect(`${systemConfig.prefixAdmin}/accounts`);
+  }
+};
+
+// [PATCH] /admin/accounts/edit/:id
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id;
+
+  // check xem tài khoản cb cập nhập có trùng với bất kì 1 tk nào trong DB?
+  const accountWithExistingEmail = await Account.findOne({
+    _id: { $ne: id}, //$ne: xét all id trừ id đang chọn
+    // để loại bỏ trường hợp khi không chỉnh sửa sẽ bị lỗi tồn tại
+    email: req.body.email,
+    deleted: false,
+  });
+
+  if (accountWithExistingEmail) {
+    req.flash("error", `Email ${req.body.email} tồn tại`);
+  } else {
+    // xử lý user vẫn để password trống sẽ giữ password cũ
+    if(req.body.password){
+      req.body.password = md5(req.body.password);
+    } else {
+      delete req.body.password; //xóa key password trong body
+    }
+
+    await Account.updateOne({_id: id}, req.body);
+
+    req.flash("success", "Cập nhập tài khoản thành công!");
+
+  }
+  res.redirect("back");
+};
--- validates/admin/account.validate.js
+++ validates/admin/account.validate.js
@@ -20,3 +20,20 @@ module.exports.createPost = (req, res, next) => {
   
   next(); // next sang bước kế tiếp
 }
+
+// hàm này khi bắt lỗi khi user dùng f12 xóa đi require trong tiêu đề
+module.exports.editPatch = (req, res, next) => {
+  if(!req.body.fullName){
+      req.flash("error", "Vui lòng nhập họ tên!");
+      res.redirect("back");
+      return;
+  }
+
+  if(!req.body.email){
+    req.flash("error", "Vui lòng nhập email!");
+    res.redirect("back");
+    return;
+  }
+  
+  next(); // next sang bước kế tiếp
+}
