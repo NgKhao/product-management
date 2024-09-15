f48511bcc4ce5f73693f13478dcd394f6e69c47d
NgKhao <bsbnk141@gmail.com>
2024-09-15 15:57:38 +0700

    add button go back for product detail page

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,27 @@
-// console.log("ok")
+// Show Alert
+const showAlert = document.querySelector("[show-alert]");
+if (showAlert) {
+  const time = parseInt(showAlert.getAttribute("data-time"));
+  const closeAlert = showAlert.querySelector("[close-alert]");
+
+  setTimeout(() => {
+    showAlert.classList.add("alert-hidden");
+  }, time);
+
+  closeAlert.addEventListener("click", () => {
+    showAlert.classList.add("alert-hidden");
+  });
+}
+// End Show Alert
+
+// button go back
+// buttonGoBack return 1 arr nếu có nhiều button go back
+const buttonGoBack = document.querySelectorAll("[button-go-back]");
+if (buttonGoBack.length > 0) {
+  buttonGoBack.forEach((button) => {
+    button.addEventListener("click", () => {
+      history.back();
+    });
+  });
+}
+//End button go back
