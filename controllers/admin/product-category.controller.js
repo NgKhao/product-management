87b592e86f93c1f02d391f69af60e6d99510b216
NgKhao <bsbnk141@gmail.com>
2024-09-18 22:31:19 +0700

    fix a few bugs on the admin side

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -41,8 +41,8 @@ module.exports.create = async (req, res) => {
 // [POST] /admin/products-category/create
 module.exports.createPost = async (req, res) => {
   const permissions = res.locals.role.permissions;
-// permissions.includes("products-category_create": check không cho tạo trực tiếp bên postman
-  if (permissions.includes("products-category_create")) {
+// permissions.includes("product-category_create": check không cho tạo trực tiếp bên postman
+  if (permissions.includes("product-category_create")) {
     if (req.body.position == "") {
       const count = await ProductCategory.countDocuments();
       req.body.position = count + 1;
