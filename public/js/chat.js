b4016ea9710e3b7ca3669850a0d0109926e642b3
NgKhao <bsbnk141@gmail.com>
2024-10-16 22:43:55 +0700

    bulding a basic chat feature

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,15 @@
-var socket = io();
+//CLIENT_SEND_MESSAGE
+const formSendData = document.querySelector(".chat .inner-form");
+if(formSendData){
+  formSendData.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // content của thuộc tính name
+    const content = e.target.elements.content.value;
+
+    if(content) {
+      socket.emit("CLIENT_SEND_MESSAGE", content); //client phát data
+      e.target.elements.content.value = "";
+    }
+  })
+} 
+//END CLIENT_SEND_MESSAGE
