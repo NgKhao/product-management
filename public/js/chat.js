4e536dbf647ffc768b238c24c254fb3af07345d7
NgKhao <bsbnk141@gmail.com>
2024-10-17 00:29:52 +0700

    show data realtime

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,42 @@
 //CLIENT_SEND_MESSAGE
 const formSendData = document.querySelector(".chat .inner-form");
-if(formSendData){
+if (formSendData) {
   formSendData.addEventListener("submit", (e) => {
     e.preventDefault();
     // content của thuộc tính name
     const content = e.target.elements.content.value;
 
-    if(content) {
+    if (content) {
       socket.emit("CLIENT_SEND_MESSAGE", content); //client phát data
       e.target.elements.content.value = "";
     }
-  })
-} 
+  });
+}
 //END CLIENT_SEND_MESSAGE
+
+//SERVER RETURN_MESSAGE
+socket.on("SERVER_RETURN_MESSAGE", (data) => {
+  // Thêm khối div chứa message đã gửi ra giao diện
+
+  const myId = document.querySelector("[my-id]").getAttribute("my-id");
+  const body = document.querySelector(".chat .inner-body");
+
+  const div = document.createElement("div");
+
+  let htmlFullName = "";
+
+  // check nếu không phải user loggin thì thên name vào
+  if (myId == data.userId) {
+    div.classList.add("inner-outgoing");
+  } else {
+    div.classList.add("inner-incoming");
+    htmlFullName = `<div class="inner-name">${data.fullName}</div>`;
+  }
+  div.innerHTML = `
+    ${htmlFullName}
+    <div class="inner-content">${data.content}</div>
+  `;
+  // thêm thẻ vào cuối
+  body.appendChild(div);
+});
+//END_SERVER RETURN_MESSAGE
