864788b102de87befc872758b4fd43b2b004101e
NgKhao <bsbnk141@gmail.com>
2024-10-16 15:13:34 +0700

    add chat interface

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -9,6 +9,8 @@ const searchRoutes = require("./search.route");
 const cartRoutes = require("./cart.route");
 const checkoutRoutes = require("./checkout.route");
 const userRoutes = require("./user.route");
+const chatRoutes = require("./chat.route");
+
 
 module.exports = (app) => {
   // app.use(categoryMiddleWare.category): app sẽ use hàm category cho all các route ở dưới
@@ -33,4 +35,7 @@ module.exports = (app) => {
   app.use("/checkout", checkoutRoutes);
 
   app.use("/user", userRoutes);
+
+  app.use("/chat", chatRoutes);
+
 };
