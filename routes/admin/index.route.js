e137ef4309015c6fc68472d3d81bfb48ce7fb308
NgKhao <bsbnk141@gmail.com>
2024-11-23 12:48:12 +0700

    fix /admin/

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -18,8 +18,12 @@ const myAccountRoutes = require("./my-account.route");
 
 const settingRoutes = require("./setting.route");
 
+const authController = require("../../controllers/admin/auth.controller");
+
 module.exports = (app) => {
   const PATH_ADMIN = systemConfig.prefixAdmin;
+
+  app.get(PATH_ADMIN + "/", authController.login);
 
   app.use(
     PATH_ADMIN + "/dashboard",
